a2716bd4ac81b9fbd564e44e0b61ec1336e7b816
nazmul <nazmul@maptaskr.com>
2020-01-06 00:49:23 +0600

    db update

--- db/db.js
+++ db/db.js
@@ -2,9 +2,9 @@ var mysql      = require('mysql');
 
 const pool = mysql.createPool({
   host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'tanvorhw_bluecrm'
+  user     : 'tanvorhw_t33user',
+  password : 'Tanvir786',
+  database : 'tanvorhw_nodeApi'
 });
 
 module.exports = pool;
