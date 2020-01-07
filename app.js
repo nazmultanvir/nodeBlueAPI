7795cc88502f9b2e32933e9c6f50ebcc9fe58a43
nazmul <nazmul@maptaskr.com>
2020-01-07 06:41:34 +0600

    cors model chnaged

--- app.js
+++ app.js
@@ -9,13 +9,27 @@ const agentRoute = require('./router/agentRouter')
  const userRoute = require('./router/userRoute')
 //express and body parser
 const app = express()
-
-app.use(cors({origin: '*'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 //agent route
