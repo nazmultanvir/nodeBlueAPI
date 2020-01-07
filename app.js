28eac73dcaab54441efba1940d08415fedce8797
nazmul <nazmul@maptaskr.com>
2020-01-07 06:19:05 +0600

    added cors in project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 
 //file import
@@ -10,6 +11,8 @@ const agentRoute = require('./router/agentRouter')
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors());
 
 
 
